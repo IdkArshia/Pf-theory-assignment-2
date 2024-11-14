5b8d028bb2a0af2b77bec34afa0d28f2d2837f63
IdkArshia <arshia.azeem.0511@gmail.com>
2024-11-14 21:11:34 +0500

    Add files via upload

---
+++ theoryq6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int matchsticks(int n) {
+    if (n % 5 == 0) {
+        return -1; 
+    } else {
+        return n % 5; 
+    }
+}
+
+int main() {
+    int n,result; 
+    printf("Enter the number of matchsticks on the table: ");
+    scanf("%d", &n);
+    result= matchsticks(n);
+    if (result == -1) {
+        printf("it is impossible for A to win the game");
+    } else {
+        printf("Player A should pick %d matchsticks on the first turn to guarantee a win\n", result);
+    }
+
+    return 0;
+}
